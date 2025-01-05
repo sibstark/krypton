a92e8eace91a13279694d537135c71e46a58f7d4
Oleg <o.bruch@icloud.com>
2025-01-05 11:48:10 +0400

    bun

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    server: {
+        port: Number(process.env.PORT || 3000)
+    }
+});
