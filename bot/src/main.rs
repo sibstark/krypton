81a9173b8df3609f013b4b16e17db2a58dc2bd6e
Oleg <o.bruch@icloud.com>
2025-01-31 19:31:57 +0400

    progress

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,8 +1,25 @@
 use dotenv::dotenv;
-use teloxide::{prelude::*, utils::command::BotCommands};
+use serde::{Deserialize, Serialize};
+use teloxide::{
+    dispatching::dialogue::{GetChatId, InMemStorage},
+    prelude::*,
+    utils::command::BotCommands,
+};
 mod krypton_bot;
 use krypton_bot::KryptonBot;
 use std::env::var;
+
+#[derive(Clone, Default, Serialize, Deserialize)]
+enum DialogState {
+    #[default]
+    Idle,
+    InStartFlow(StartState), // Добавлять новые состояния команд здесь
+}
+#[derive(Clone, Default, Serialize, Deserialize)]
+enum StartState {
+    #[default]
+    Greeting,
+}
 
 #[tokio::main]
 async fn main() {
@@ -19,7 +36,10 @@ async fn main() {
 }
 
 #[derive(BotCommands, Clone)]
-#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
 enum Command {
     #[command(description = "Show list of commands")]
     Help,
@@ -41,20 +61,52 @@ enum Command {
 
 async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
     match cmd {
-        Command::Start => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
-        Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
+        Command::Start => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+        Command::Help => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?
+        }
         Command::Username(username) => {
-            bot.send_message(msg.chat.id, format!("Your username is @{username}.")).await?
+            bot.send_message(msg.chat.id, format!("Your username is @{username}."))
+                .await?
         }
         Command::UsernameAndAge { username, age } => {
-            bot.send_message(msg.chat.id, format!("Your username is @{username} and age is {age}."))
-                .await?
-        },
-        _ => {
-            bot.send_message(msg.chat.id, format!("Not implemented"))
+            bot.send_message(
+                msg.chat.id,
+                format!("Your username is @{username} and age is {age}."),
+            )
             .await?
         }
+        _ => {
+            bot.send_message(msg.chat.id, format!("Not implemented"))
+                .await?
+        }
     };
+
+    Ok(())
+}
+
+async fn command_handler(
+    bot: Bot,
+    msg: Message,
+    cmd: Command,
+    dialogue: Dialogue<InMemStorage<DialogState>>,
+) -> ResponseResult<()> {
+    match cmd {
+        Command::Start => handle_start(bot, msg, dialogue).await,
+        // Command::Find => handle_find(bot, msg, dialogue).await,
+        // Command::Pay => handle_pay(bot, msg, dialogue).await,
+        // Обработчики для других команд
+        _ => handle_unknown_command(bot, msg).await,
+    }
+}
+
+async fn handle_unknown_command(bot: Bot, msg: Message) -> ResponseResult<()> {
+    bot.send_message(msg.chat.id, format!("Unknow command, try /start"))
+    .await?;
 
     Ok(())
 }
