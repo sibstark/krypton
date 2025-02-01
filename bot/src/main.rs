e59545eb15a180cfd30f28e8ed58ac7a107466bc
Oleg <o.bruch@icloud.com>
2025-02-01 14:28:42 +0400

    progress

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -5,7 +5,6 @@ use teloxide::{
         dialogue::{Dialogue, InMemStorage},
         DpHandlerDescription,
     },
-    filter_command,
     prelude::*,
     utils::command::BotCommands,
     RequestError,
@@ -19,8 +18,9 @@ use std::sync::Arc;
 enum DialogState {
     #[default]
     Idle,
-    InStartFlow(StartState), // Добавлять новые состояния команд здесь
+    InStartFlow(StartState),
 }
+
 #[derive(Clone, Default, Serialize, Deserialize)]
 enum StartState {
     #[default]
@@ -31,17 +31,16 @@ enum StartState {
 async fn main() {
     pretty_env_logger::init();
     log::info!("Starting throw dice bot...");
-    dotenv().ok(); // Загрузит переменные из .env файла
+    dotenv().ok();
     let token = var("TELOXIDE_TOKEN").unwrap();
     let db_connection = var("DATABASE_URL").unwrap();
     print!("token {}", token);
 
     let bot = KryptonBot::new(token, db_connection).await.unwrap();
+    let storage = InMemStorage::<DialogState>::new();
 
-    let storage = InMemStorage::<DialogState>::new(); // Создаём Arc
-
-    Dispatcher::builder(bot.bot.clone(), handler()) // bot.bot передаётся в Dispatcher
-        .dependencies(dptree::deps![storage.clone()]) // Передаём storage без двойного Arc
+    Dispatcher::builder(bot.bot, handler())
+        .dependencies(dptree::deps![storage])
         .enable_ctrlc_handler()
         .build()
         .dispatch()
@@ -64,49 +63,21 @@ enum Command {
     Pay,
     #[command(description = "My subscriptions")]
     Subscriptions,
-    // delete then
     #[command(description = "handle a username.")]
     Username(String),
-    // delete then
     #[command(description = "handle a username and an age.", parse_with = "split")]
     UsernameAndAge { username: String, age: u8 },
 }
 
-async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    match cmd {
-        Command::Start => {
-            bot.send_message(msg.chat.id, Command::descriptions().to_string())
-                .await?
-        }
-        Command::Help => {
-            bot.send_message(msg.chat.id, Command::descriptions().to_string())
-                .await?
-        }
-        Command::Username(username) => {
-            bot.send_message(msg.chat.id, format!("Your username is @{username}."))
-                .await?
-        }
-        Command::UsernameAndAge { username, age } => {
-            bot.send_message(
-                msg.chat.id,
-                format!("Your username is @{username} and age is {age}."),
-            )
-            .await?
-        }
-        _ => {
-            bot.send_message(msg.chat.id, format!("Not implemented"))
-                .await?
-        }
-    };
-
-    Ok(())
-}
-
 fn handler() -> Handler<'static, DependencyMap, Result<(), RequestError>, DpHandlerDescription> {
     dptree::entry().branch(
-        Update::filter_message()
-            .enter_dialogue::<Message, InMemStorage<DialogState>, DialogState>()
-            .branch(filter_command::<Command, ResponseResult<()>>().endpoint(command_handler)),
+        Update::filter_message().branch(
+            dptree::entry()
+                .filter_command::<Command>()
+                .endpoint(command_handler)
+        ).branch(        Update::filter_message().branch(
+            dptree::entry()
+                .endpoint(handle_regular_message)))
     )
 }
 
@@ -114,13 +85,12 @@ async fn command_handler(
     bot: Bot,
     msg: Message,
     cmd: Command,
-    dialogue: Dialogue<InMemStorage<DialogState>, DialogState>,
+    dialogue_storage: Arc<InMemStorage<DialogState>>,
 ) -> ResponseResult<()> {
+    let dialogue = Dialogue::new(dialogue_storage, msg.chat.id);
+
     match cmd {
         Command::Start => handle_start(bot, msg, dialogue).await,
-        // Command::Find => handle_find(bot, msg, dialogue).await,
-        // Command::Pay => handle_pay(bot, msg, dialogue).await,
-        // Обработчики для других команд
         _ => handle_unknown_command(bot, msg).await,
     }
 }
@@ -128,7 +98,7 @@ async fn command_handler(
 async fn handle_start(
     bot: Bot,
     msg: Message,
-    dialogue: Dialogue<InMemStorage<DialogState>, DialogState>,
+    dialogue: Dialogue<DialogState, InMemStorage<DialogState>>,
 ) -> ResponseResult<()> {
     bot.send_message(msg.chat.id, Command::descriptions().to_string())
         .await?;
@@ -137,8 +107,18 @@ async fn handle_start(
 }
 
 async fn handle_unknown_command(bot: Bot, msg: Message) -> ResponseResult<()> {
-    bot.send_message(msg.chat.id, format!("Unknow command, try /start"))
+    bot.send_message(msg.chat.id, "Unknown command, try /start")
         .await?;
+
+    Ok(())
+}
+
+async fn handle_regular_message(bot: Bot, msg: Message) -> ResponseResult<()> {
+    bot.send_message(
+        msg.chat.id,
+        "This is not a command. Please use commands like /start or /help",
+    )
+    .await?;
 
     Ok(())
 }
